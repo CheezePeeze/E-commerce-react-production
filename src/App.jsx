31170fee89f6f7041c4bf6465136e580dd768107
VladysslavTkach <tkachacc.program@gmail.com>
2024-02-08 11:39:34 +0100

    fixing the tailwind css rules

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-
+import LoginFormPage from './pages/LoginFormPage';
+import './index.css';
 function App() {
 	return (
-			<Routes>
-				<Route path="/" element={<Home />} />
-			</Routes>
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="/login" element={<LoginFormPage />} />
+		</Routes>
 	);
 }
 
