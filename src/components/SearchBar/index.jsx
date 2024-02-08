27068ac633d0a2e36c6dbcc2c0005411dda6add2
Maksym Synytskyi <citomax7196@gmail.com>
2024-02-08 16:37:08 +0100

    search bar start logic

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,24 +1,18 @@
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 
-const SearchBar = () => {
-  const top100Films = [
-    { label: 'The Shawshank Redemption', year: 1994 },
-    { label: 'The Godfather', year: 1972 },
-    { label: 'The Godfather: Part II', year: 1974 },
-    { label: 'The Dark Knight', year: 2008 },
-    { label: '12 Angry Men', year: 1957 },
-    { label: "Schindler's List", year: 1993 },
-    { label: 'Pulp Fiction', year: 1994 },
-  ]
+const SearchBar = ({ items, searchHandle }) => {
+
+
   return (
     <div className='flex justify-center'>
       <Autocomplete
         disablePortal
         id="combo-box-demo"
-        options={top100Films}
-        sx={{ width: '80%' }}
-        renderInput={(params) => <TextField {...params} label="Movie" />}
+        options={items}
+        sx={{ width: '85%' }}
+        renderInput={(params) => <TextField {...params} label="Search" />}
+        onInputChange={(e, newInputValue) => searchHandle(newInputValue)}
       />
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,17 +5,24 @@ import axios from 'axios'
 import SearchBar from '../components/SearchBar'
 
 const Home = () => {
+  const [searchItems, setSearchItems] = useState([])
   const [randomItems, setRandomItems] = useState([])
   useEffect(() => {
     axios.get('https://json-server-shop.adaptable.app/items')
       .then(res => setRandomItems(res.data))
   }, [])
 
+  const searchHandle = async (value) => {
+    if (value.length >= 2) {
+      axios.get('https://json-server-shop.adaptable.app/items')
+        .then(res => console.log(res.data))
+    }
+  }
 
   return (
     <div className='container mx-auto'>
       <Navbar />
-      <SearchBar />
+      <SearchBar items={searchItems} searchHandle={searchHandle} />
       <MultiCarousel items={randomItems} />
     </div>
   )
