03fb3f1a577963a1cf873f7c230318aa0b1d2ec4
Maksym Synytskyi <citomax7196@gmail.com>
2024-02-09 10:07:16 +0100

    search bar tiny fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,32 +8,27 @@ import SearchBar from '../components/SearchBar'
 const Home = () => {
   const [searchItems, setSearchItems] = useState([])
   const [randomItems, setRandomItems] = useState([])
-  const [loading, setLoading] = useState(true)
   useEffect(() => {
     axios.get('https://json-server-shop.adaptable.app/items')
       .then(res => setRandomItems(res.data))
       .catch((err) => console.error(err))
-      .finally(() => setLoading(false))
   }, [])
 
-  const searchHandle = async (value) => {
-    const originalArr = await axios.get('https://json-server-shop.adaptable.app/items')
-      .then(res => res.data)
-    // const filteredArr = originalArr.filter(item => item.title.toLowerCase().includes(value.toLowerCase()))
-    const test = originalArr.map(item => ({
-      ...item,
-      label: item.title
-    }))
-    setSearchItems(test)
-    // console.log(originalArr);
-
+  const searchHandle = async () => {
+    try {
+      const { data } = await axios.get('https://json-server-shop.adaptable.app/items');
+      const newArr = data.map(item => ({ ...item, label: item.title }));
+      setSearchItems(newArr);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
   }
 
   return (
     <div className='container mx-auto'>
       <Navbar />
       <SearchBar items={searchItems} searchHandle={searchHandle} />
-      {!loading && <MultiCarousel items={randomItems} />}
+      <MultiCarousel items={randomItems} />
     </div>
   )
 }
