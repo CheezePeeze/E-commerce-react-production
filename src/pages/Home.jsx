2dbeab4a6f9196bcaee2c1944396b0966d6f52b7
Maksym Synytskyi <citomax7196@gmail.com>
2024-02-14 12:36:41 +0100

    card logic

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,9 +5,9 @@ import CardItem from '../components/Card'
 import axios from 'axios'
 import SearchBar from '../components/SearchBar'
 import CircularProgressBar from '../components/CircularProgressBar'
-import { getProductsFakeStoreApi, getProductsDummyApi, getProductsByQueryDummyApi, getAllProductsByQuery, getCategoriesFakeStoreApi, getCategoriesDummyApi, getCategoryByQueryFakeStoreApi } from '../common/api'
+import { getProductsFakeStoreApi, getProductsDummyApi, getProductsByQueryDummyApi, getAllProductsByQuery, getCategoriesFakeStoreApi, getCategoriesDummyApi, getCategoryByQueryFakeStoreApi, getCategoryByQueryDummyApi } from '../common/api'
 import { UNIQ_ID } from '../common/constants'
-
+import _ from 'lodash';
 
 const Home = () => {
   const [searchItems, setSearchItems] = useState([])
@@ -17,8 +17,9 @@ const Home = () => {
   const [isLoad, setIsLoad] = useState(true)
 
   useEffect(() => {
-    // getCategories()
-    getDataToCard()
+    getDataForCardByCategory()
+    getDataForCarouselByCategory()
+    // getDataToCard()
     getItems()
   }, [])
   // console.log(import.meta.env.VITE_SPOON_KEY);
@@ -79,22 +80,70 @@ const Home = () => {
 
   // console.log(items);
 
-  const getDataToCard = async () => {
 
-    const { data: arrOfMinCategories } = await getCategoriesFakeStoreApi()
-    const t = arrOfMinCategories.slice(0, 4).map(async (category) => {
-      return { data } = await getCategoryByQueryFakeStoreApi(category)
-    })
 
-    console.log(t);
-    // getCategoriesDummyApi().then(res => res.data[Math.floor(Math.random() * res.data.length)])
+  const getDataForCardByCategory = async () => {
 
-  }
+    const { data } = await getCategoriesFakeStoreApi();
+    const categoryPromises = data.slice(0, 4).map(async (category) => {
+      const res = await getCategoryByQueryFakeStoreApi(category);
+      return res.data.slice(0, 3);
+    });
 
-  const getDataToCarousel = async () => {
-    const { data: category } = await getCategoriesDummyApi().then(res => res.data[Math.floor(Math.random() * res.data.length)])
+    const itemsForCardArray = await axios.all(categoryPromises);
+    setItemsForCard(itemsForCardArray);
+  };
 
-  }
+
+
+  // const getDataForCarouselByCategory = async () => {
+  //   const { data } = await getCategoriesDummyApi();
+  //   const randomCategories = _.sampleSize(data, 8);
+  //   const categoryPromises = randomCategories.map(async (category) => {
+  //     const res = await getCategoryByQueryDummyApi(category);
+  //     return res.data.products;
+  //   });
+  //   const itemsArrays = await Promise.all(categoryPromises);
+
+  //   const reducedArr = []
+  //   for (let i = 0; i < itemsArrays.length; i += 2) {
+  //     test.push([...itemsArrays[i], ...itemsArrays[i + 1]]);
+  //   }
+  //   setItemsForCarousel(reducedArr);
+  // };
+
+  const getDataForCarouselByCategory = async () => {
+    const { data } = await getCategoriesDummyApi();
+    const randomCategories = _.sampleSize(data, 8);
+    const categoryPromises = randomCategories.map(async (category) => (await getCategoryByQueryDummyApi(category)).data.products);
+    const itemsArrays = await Promise.all(categoryPromises);
+    const reducedArr = itemsArrays.reduce((acc, items, i) => {
+      if (i % 2 === 0) {
+        acc.push([...items, ...(itemsArrays[i + 1] || [])]);
+      }
+      return acc;
+    }, []);
+    setItemsForCarousel(reducedArr);
+  };
+
+
+
+  // const getDataToCard = async () => {
+
+  //   const { data: arrOfMinCategories } = await getCategoriesFakeStoreApi()
+  //   const t = arrOfMinCategories.slice(0, 4).map(async (category) => {
+  //     return { data } = await getCategoryByQueryFakeStoreApi(category)
+  //   })
+
+  //   console.log(t);
+  //   // getCategoriesDummyApi().then(res => res.data[Math.floor(Math.random() * res.data.length)])
+
+  // }
+
+  // const getDataToCarousel = async () => {
+  //   const { data: category } = await getCategoriesDummyApi().then(res => res.data[Math.floor(Math.random() * res.data.length)])
+
+  // }
 
 
   // const filterItemByCategory = (categoryId, max = Infinity) => {
@@ -108,7 +157,7 @@ const Home = () => {
   // const itemForSingleCard = () => {
   //   return getRandomItem(items)
   // }
-
+  console.log(itemsForCard);
   return (
     <div className='black-background black-color min-h-screen'>
       <div className='container mx-auto '>
@@ -118,6 +167,13 @@ const Home = () => {
           : (
             <>
               <SearchBar options={searchItems} searchHandle={searchHandle} />
+              {itemsForCard.length > 0 && itemsForCard.map((cardItems, idx) => (
+                <div className='grid grid-cols-3' key={idx}>
+                  {cardItems.map(item => (
+                    <CardItem key={item.id} item={item} />
+                  ))}
+                </div>
+              ))}
               {/* <MultiCarousel items={filterItemByCategory(category.id, 10)} title={category.title} /> */}
 
               {/* {(categories.length > 1 && items.length > 1) && categories.map(category => (
