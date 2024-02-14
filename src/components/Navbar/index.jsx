4b744a73d5ffe8d0a480ad62913225cef8733788
VladysslavTkach <tkachacc.program@gmail.com>
2024-02-14 16:45:50 +0100

    set a bg transparent color for navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -53,19 +53,19 @@ function classNames(...classes) {
 
 export default function Example() {
 	return (
-		<Disclosure as="nav" className="black-background">
+		<Disclosure as="nav" className=" bg-transparent">
 			<>
 				<div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
 					<div className="relative flex h-16 items-center justify-between">
 						<div className="flex items-center justify-center sm:items-stretch sm:justify-start">
 							<div className="flex flex-shrink-0 items-center">
-                <Link to='/'>
-								<img
-									className="h-8 w-auto"
-									src="https://docs.material-tailwind.com/img/logo-ct-dark.png"
-									alt="Your Company"
-								/>
-                </Link>
+								<Link to="/">
+									<img
+										className="h-8 w-auto"
+										src="https://docs.material-tailwind.com/img/logo-ct-dark.png"
+										alt="Your Company"
+									/>
+								</Link>
 							</div>
 							<div className="hidden sm:ml-6 sm:block"></div>
 						</div>
