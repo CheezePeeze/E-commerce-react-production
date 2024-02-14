f38279d9b71e0a692a22e1d1ed3fed97390293b4
VladysslavTkach <tkachacc.program@gmail.com>
2024-02-14 17:18:15 +0100

    removing commented line

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,44 +1,4 @@
-// import logo from '../../assets/logo.png';
-// import Menu from '@mui/material/Menu';
-// import MenuItem from '@mui/material/MenuItem';
-// import { useState } from 'react';
-// import profileLogo from '../../assets/profile-simple.svg'
-// import { Link } from 'react-router-dom';
-
-// const Navbar = () => {
-//   const [anchorEl, setAnchorEl] = useState(null);
-//   const open = Boolean(anchorEl);
-//   const handleClick = (event) => {
-//     setAnchorEl(event.currentTarget);
-//   };
-//   const handleClose = () => {
-//     setAnchorEl(null);
-//   };
-//   return (
-//     <div className=' flex justify-between items-center p-5'>
-//       <Link to='/'>
-//         <img src={logo} width={200} />
-//       </Link>
-//       <div>
-//         <img src={profileLogo} onClick={handleClick} className='rounded-full hover:bg-gray-300 ' width='40' />
-//         <Menu
-//           id="basic-menu"
-//           anchorEl={anchorEl}
-//           open={open}
-//           onClose={handleClose}
-//           MenuListProps={{
-//             'aria-labelledby': 'basic-button',
-//           }}
-//         >
-//           <MenuItem onClick={handleClose}>Profile</MenuItem>
-//           <MenuItem onClick={handleClose}>Logout</MenuItem>
-//         </Menu>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default Navbar\
+// import
 
 import { Fragment } from 'react';
 import { Disclosure, Menu, Transition } from '@headlessui/react';
