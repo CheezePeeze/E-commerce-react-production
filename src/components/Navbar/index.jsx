e2fc3d4785f6d66bf5fa0c22caf951b69248cc13
VladysslavTkach <tkachacc.program@gmail.com>
2024-02-15 15:59:38 +0100

    adding a cart section

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -30,28 +30,30 @@ export default function Example() {
 							<div className="hidden sm:ml-6 sm:block"></div>
 						</div>
 						<div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-							<button
-								type="button"
-								className="relative rounded-full p-1 text-black focus:outline-none"
-							>
-								<span className="absolute -inset-1.5" />
-								<span className="sr-only">View notifications</span>
-								{/* <BellIcon className="h-6 w-6" aria-hidden="true" /> */}
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									strokeWidth="1.5"
-									stroke="currentColor"
-									className="w-6 h-6"
+							<Link to="/checkout">
+								<button
+									type="button"
+									className="relative rounded-full p-1 text-black focus:outline-none"
 								>
-									<path
-										strokeLinecap="round"
-										strokeLinejoin="round"
-										d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
-									/>
-								</svg>
-							</button>
+									<span className="absolute -inset-1.5" />
+									<span className="sr-only">View cart</span>
+									{/* <BellIcon className="h-6 w-6" aria-hidden="true" /> */}
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										fill="none"
+										viewBox="0 0 24 24"
+										strokeWidth="1.5"
+										stroke="currentColor"
+										className="w-6 h-6"
+									>
+										<path
+											strokeLinecap="round"
+											strokeLinejoin="round"
+											d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+										/>
+									</svg>
+								</button>
+							</Link>
 
 							{/* Profile dropdown */}
 							<Menu as="div" className="relative ml-3">
@@ -91,14 +93,15 @@ export default function Example() {
 										</Menu.Item>
 										<Menu.Item>
 											{({ active }) => (
-												<Link to="/sign-up"
-														href="#"
-														className={classNames(
-															active ? 'bg-gray-100' : '',
-															'block px-4 py-2 text-sm text-gray-700'
-														)}
-													>
-														Sign up
+												<Link
+													to="/sign-up"
+													href="#"
+													className={classNames(
+														active ? 'bg-gray-100' : '',
+														'block px-4 py-2 text-sm text-gray-700'
+													)}
+												>
+													Sign up
 												</Link>
 											)}
 										</Menu.Item>
