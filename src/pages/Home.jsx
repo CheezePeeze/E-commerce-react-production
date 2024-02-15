60b50d7cd52a36e46f42cccb12dac253b8426db1
Maksym Synytskyi <citomax7196@gmail.com>
2024-02-15 12:24:11 +0100

    loader for home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -55,68 +55,68 @@ const Home = () => {
 		});
 	};
 
-  const getDataForCardByCategory = async () => {
-    const { data } = await getCategoriesFakeStoreApi();
-    const categoryPromises = data.slice(0, 4).map(async (category) => {
-      const res = await getCategoryByQueryFakeStoreApi(category);
-      return res.data.slice(0, 3).map(el => ({
-        ...el,
-        id: el.id,
-        rating: el.rating.rate,
-        stock: el.rating.count,
-        thumbnail: el.image,
-        images: [el.image],
-        shop: 1
-      }))
-    });
+	const getDataForCardByCategory = async () => {
+		const { data } = await getCategoriesFakeStoreApi();
+		const categoryPromises = data.slice(0, 4).map(async (category) => {
+			const res = await getCategoryByQueryFakeStoreApi(category);
+			return res.data.slice(0, 3).map(el => ({
+				...el,
+				id: el.id,
+				rating: el.rating.rate,
+				stock: el.rating.count,
+				thumbnail: el.image,
+				images: [el.image],
+				shop: 1
+			}))
+		});
 
 		const itemsForCardArray = await axios.all(categoryPromises);
 		setDataForCard(itemsForCardArray);
 	};
 
-  const getDataForCarouselByCategory = async () => {
-    const { data } = await getCategoriesDummyApi();
-    const randomCategories = _.sampleSize(data, 8);
-    const categoryPromises = randomCategories.map(async (category) => (await getCategoryByQueryDummyApi(category)).data.products);
-    const itemsArrays = await Promise.all(categoryPromises);
-    const reducedArr = itemsArrays.reduce((acc, items, i) => {
-      if (i % 2 === 0) {
-        acc.push([...items, ...(itemsArrays[i + 1] || [])]);
-      }
-      return acc;
-    }, []);
-    const mutatedItems = reducedArr.map(arr => arr.map(item => ({
-      ...item,
-      shop: 2
-    })))
-    setDataForCarousel(mutatedItems);
-  };
+	const getDataForCarouselByCategory = async () => {
+		const { data } = await getCategoriesDummyApi();
+		const randomCategories = _.sampleSize(data, 8);
+		const categoryPromises = randomCategories.map(async (category) => (await getCategoryByQueryDummyApi(category)).data.products);
+		const itemsArrays = await Promise.all(categoryPromises);
+		const reducedArr = itemsArrays.reduce((acc, items, i) => {
+			if (i % 2 === 0) {
+				acc.push([...items, ...(itemsArrays[i + 1] || [])]);
+			}
+			return acc;
+		}, []);
+		const mutatedItems = reducedArr.map(arr => arr.map(item => ({
+			...item,
+			shop: 2
+		})))
+		setDataForCarousel(mutatedItems);
+	};
 
-  return (
-    <div className='black-background black-color min-h-screen'>
-        <Navbar />
-      <div className='container mx-auto my-5 '>
-        {isLoad ?
-          <CircularProgressBar />
-          : (
-            <>
-              <SearchBar options={searchItems} searchHandle={searchHandle} />
-              {dataForCard.length > 0 && dataForCard.map((cardItems, idx) => (
-                <div key={idx}>
-                  <div className='grid grid-cols-3' >
-                    {cardItems.map(item => (
-                      <CardItem key={item.id} item={item} />
-                    ))}
-                  </div>
-                  {dataForCarousel[idx] && <MultiCarousel items={dataForCarousel[idx]} />}
-                </div>
-              ))}
-            </>
-          )}
-      </div>
-      <FooterWithLogo/>
-    </div>
-  )
+	return (
+		<div className='black-background black-color min-h-screen'>
+			{isLoad ?
+				<CircularProgressBar />
+				: (
+					<>
+						<Navbar />
+						<div className='container mx-auto my-5 '>
+							<SearchBar options={searchItems} searchHandle={searchHandle} />
+							{dataForCard.length > 0 && dataForCard.map((cardItems, idx) => (
+								<div key={idx}>
+									<div className='grid grid-cols-3' >
+										{cardItems.map(item => (
+											<CardItem key={item.id} item={item} />
+										))}
+									</div>
+									{dataForCarousel[idx] && <MultiCarousel items={dataForCarousel[idx]} />}
+								</div>
+							))}
+						</div>
+						<FooterWithLogo />
+					</>
+				)}
+		</div>
+	)
 }
 
 export default Home
